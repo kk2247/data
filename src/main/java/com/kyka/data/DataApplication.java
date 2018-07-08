5b0c2d58ac717a1ed92b3e35d458334599d6ec3a
kk <2247839764@qq.com>
2018-07-08 15:02:46 +0800

    数据结构实验，利用springboot整合jsp实现的web项目

---
+++ src/main/java/com/kyka/data/DataApplication.java
@@ -0,0 +1,13 @@
+package com.kyka.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DataApplication  extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataApplication.class, args);
+    }
+}
